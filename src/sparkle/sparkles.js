74d6888f9c790f66c9e6203650a4abb3b497bb21
yun900800@126.com <yun900800@126.com>
2023-07-10 16:27:34 +0800

    test(react-custom/sparkle/sparkle.test)
    1. 增加一个对sparkles组件的测试类
    2. 使用RTL对react组件进行测试
    
    added sparkles.test.js modified sparkles.js

--- src/sparkle/sparkles.js
+++ src/sparkle/sparkles.js
@@ -4,7 +4,7 @@ import useRandomInterval from '../hooks/useRandomInterval';
 import usePrefersReducedMotion from '../hooks/usePrefersReducedMotion';
 import random from "../utils/random";
 const DEFAULT_COLOR = '#FFC700';
-const generateSparkle = color => {
+export const generateSparkle = color => {
   const sparkle = {
     id: String(random(10000, 99999)),
     createdAt: Date.now(),
@@ -50,11 +50,11 @@ const Sparkles = ({ color = DEFAULT_COLOR, children, ...delegated }) => {
     </Wrapper>
   );
 };
-const Sparkle = ({ size, color, style }) => {
+export const Sparkle = ({ size, color, style }) => {
   const path =
     'M26.5 25.5C19.0043 33.3697 0 34 0 34C0 34 19.1013 35.3684 26.5 43.5C33.234 50.901 34 68 34 68C34 68 36.9884 50.7065 44.5 43.5C51.6431 36.647 68 34 68 34C68 34 51.6947 32.0939 44.5 25.5C36.5605 18.2235 34 0 34 0C34 0 33.6591 17.9837 26.5 25.5Z';
   return (
-    <SparkleWrapper style={style}>
+    <SparkleWrapper style={style} data-testid="sparkle">
       <SparkleSvg width={size} height={size} viewBox="0 0 68 68" fill="none">
         <path d={path} fill={color} />
       </SparkleSvg>
