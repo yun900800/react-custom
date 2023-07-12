b4f0ceba6831a3d83d5ae400642920c7c8ee0c65
yun900800@126.com <yun900800@126.com>
2023-07-12 11:41:45 +0800

    feature(ts advanced) 增加ts高级类型的说明
    1.学习和理解Exclude的使用,并说明类型兼容性的原理

--- src/component/ts-demo/ts-advanced.tsx
+++ src/component/ts-demo/ts-advanced.tsx
@@ -23,7 +23,7 @@ function handleEvent(ele:Element, event: EventName){
 handleEvent(document.getElementById('hello'), 'scroll');  // 没问题
 // handleEvent(document.getElementById('world'), 'dblclick'); // 报错，event 不能为 'dblclick'.
 
-// 数组合并相同类型对象，元素合并不同类型对象
+// 数组合并相同类型对象，元组合并不同类型对象
 let tom: [string, number] = ['Tom', 25];
 tom[0].slice(1);
 tom[1].toFixed(2);
@@ -41,4 +41,168 @@ let tom2: [string, number];
 tom2 = ['Tom', 25];
 tom2.push('male');
 // tom2.push(true); 编译报错
-console.log(tom2);
+//console.log(tom2);
+
+// es6中类的实现
+class Animal {
+    //name;
+    constructor(name) {
+        this.name = name;
+    }
+    sayHi() {
+        return `My name is ${this.name}`;
+    }
+
+    static isAnimal(a) {
+        return a instanceof Animal;
+    }
+
+    set name(value:any) {
+        
+    }
+
+    get name() {
+        return this.name;
+    }
+}
+
+let a = new Animal('Jack');
+a.name = 'Kate Green';
+Animal.isAnimal(a);
+//console.log(a.sayHi());
+
+class TomCat extends Animal {
+    constructor(name) {
+        super(name);
+    }
+
+    sayHi() {
+        return 'Miyelow, '+super.sayHi();
+    }
+}
+
+let c = new TomCat('Tom'); // Tom
+// console.log(c.sayHi());
+
+// 参数属性
+class ParamAnimal {
+    // public name: string;
+    readonly age;
+    public constructor(public name, age, public readonly pass) {
+      // this.name = name;
+      this.age = age;
+    }
+}
+
+let paramAnimal = new ParamAnimal('paramAnimal',15,'6688');
+// console.log(paramAnimal.name);
+// 一下编译报错
+// paramAnimal.age = 20;
+// paramAnimal.pass = 555;
+
+//抽象类不能实例化,子类必须实现抽象方法
+
+// 泛型（Generics）是指在定义函数、接口或类的时候，不预先指定具体的类型，而在使用的时候再指定类型的一种特性
+
+// 声明合并 如果定义了两个相同名字的函数、接口或类，那么它们会合并成一个类型：
+interface Alarm {
+    price: number;
+}
+interface Alarm {
+    weight: number;
+}
+
+// 合并的属性的类型必须是唯一的：
+
+//交叉类型
+interface Ant {
+    name: string;
+    weight:number;
+}
+
+interface Fly {
+    flyWeight:number;
+    speed:number;
+}
+
+const FlyAnt: Ant & Fly = {
+    name: '',
+    weight:50,
+    flyWeight:80,
+    speed: 100
+};
+// 把所有类型叠加到一起成为一种新类型,它包含所有类型的特性
+// 交叉类型的返回类型既要符合Ant，又要符合Fly
+
+//联合类型
+class Bird {
+    fly() {
+
+    }
+    layEggs() {
+
+    }
+}
+
+class Fish {
+    swim() {
+
+    }
+    layEggs() {
+        
+    }
+}
+
+const bird = new Bird();
+const fish = new Fish();
+
+const start = function(pet: Bird | Fish) {
+    pet.layEggs();
+    // 以下编译报错
+    // pet.swim();
+    // pet.fly();
+}
+
+//type Exclude<T,U> = T extends U ? T :never;
+// https://blog.csdn.net/weixin_44051815/article/details/124182060
+
+// 类型映射
+interface Person {
+    name:string;
+    age:number;
+    gender:number;
+}
+
+type ReadOnlyType<T> =  {
+    readonly [p in keyof T] : T;
+}
+
+type ReadOnlyPerson = ReadOnlyType<Person>;
+
+// https://blog.csdn.net/Magneto7/article/details/121489097
+
+// 类型兼容性说明:确定一个类型是否可以赋值给其他类型
+// 如果x要兼容y,那么y至少要具有x相同的属性。
+
+
+// Exclude使用
+type myUnionType = "🍇" | "🍎" | "🫐" | "🍋";
+
+// This works!
+let lemon:myUnionType = "🍋"
+ 
+let noLemonsPlease:Exclude<myUnionType, "🍋"> = "🍇";
+//  ^
+//  └ - - Type is  "🍇" | "🍎" | "🫐"
+
+let noApplesOrLemons:Exclude<myUnionType, "🍋" | "🍎"> = "🍇";
+//  ^
+//  └ - - Type is  "🍇" | "🫐"
+
+let onlyRaspberries:Exclude<myUnionType, "🍋" | "🍎" | "🫐"> = "🍇";
+//  ^
+//  └ - - Type is  "🍇"
+
+let backToLemons:myUnionType = "🍋"
+//  ^
+//  └ - - Type is  "🍇" | "🍎" | "🫐" | "🍋"
--- src/sparkle/design.md
+++ src/sparkle/design.md
@@ -10,3 +10,41 @@
    - 1.2 相对于视口: 固定定位的top,left;
    - 1.3 相对于自身: translate,border-radius;
    - 1.4 相对于背景区的宽高: background-size;
+
+# 为什么要设计这样的一个sparkles组件
+
+1. 指示句子某部分重要的标签<strong>和<em>的语义表达不够充分,并且它们强调警告和极其严重的情况,缺乏一种积极活泼的指示重要.  
+2. 在使用者的角度分析此组件的使用情况,设计出具体的接口和API.
+```
+{/* It should be able to wrap images */}
+<Sparkles>
+  <img src="gold-cat.png" alt="A golden cat statue" />
+</Sparkles>
+{/* It should also be able to wrap inline text */}
+<p>
+  Next race: <Sparkles>Rainbow Road</Sparkles>.
+</p>
+```
+3. 创建一个闪烁星星的svg assets
+4. 创建生成一个sparkle具体需要的数据,包括:
+```
+{
+   id: String(random(10000, 99999)),
+   createdAt: Date.now(),
+   // Bright yellow color:
+   color,
+   size: random(10, 20),
+   style: {
+      // Pick a random spot in the available space
+      top: random(0, 100) + '%',
+      left: random(0, 100) + '%',
+      // Float sparkles above sibling content
+      zIndex: 2,
+   },
+}
+```
+5. 设计闪烁动画,包含旋转的收缩两部分,如果两个动画放在一个元素上，会很生硬，因此需要增加一个父wrapper，用于
+   承载不同的动画.  
+6. sparckles是一组sparckle,设计一组合理的清理过程,这里根据时间来过滤.  
+7. 如果没有启用动画，应该注意此组件的可访问性. 
+8. 合并以上的过程,编码实现组件. 
