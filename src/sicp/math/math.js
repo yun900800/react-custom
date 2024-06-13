05b8399c6f2cd7d32c971d2c50072b3109a76e95
yun900800@126.com <yun900800@126.com>
2024-06-13 10:49:33 +0800

    [feature] 增加一个递归,不能转化成CSP风格

--- src/sicp/math/math.js
+++ src/sicp/math/math.js
@@ -1,64 +1,62 @@
-const abs = x => x >= 0 ? x : -x;
-const greaterOrEqual = (x,y) =>!(x < y);
-const plus = (a,b) => a + b;
-const minus = (a,b) => a - b;
-const a_plus_abs_b = (a,b) => (b >= 0 ? plus : minus)(a, b);
+const abs = (x) => (x >= 0 ? x : -x)
+const greaterOrEqual = (x, y) => !(x < y)
+const plus = (a, b) => a + b
+const minus = (a, b) => a - b
+const a_plus_abs_b = (a, b) => (b >= 0 ? plus : minus)(a, b)
 
-const sqrtIter = (guess,x) => {
-    const goodEnough = (guess,x) => {
-    
-        // return abs(guess*guess - x) <= 0.001;
-        //上面的计算方法在计算较大的数和较小的数的时候会失效
-        return relativeError(guess, improve(guess,x)) < 0.01
-    }
+const sqrtIter = (guess, x) => {
+  const goodEnough = (guess, x) => {
+    // return abs(guess*guess - x) <= 0.001;
+    //上面的计算方法在计算较大的数和较小的数的时候会失效
+    return relativeError(guess, improve(guess, x)) < 0.01
+  }
 
-    const average = (x,y) => {
-        return (x+y) / 2;
-    }
+  const average = (x, y) => {
+    return (x + y) / 2
+  }
 
-    // 返回新的猜测值
-    const improve = (guess,x) => {
-       return average(guess, x/guess);
-    }
+  // 返回新的猜测值
+  const improve = (guess, x) => {
+    return average(guess, x / guess)
+  }
 
-    const relativeError = (estimate, reference) => {
-        return abs(estimate-reference) / reference;
-    }
+  const relativeError = (estimate, reference) => {
+    return abs(estimate - reference) / reference
+  }
 
-    if (goodEnough(guess,x)) {
-        return guess;
-    }
-    return sqrtIter(improve(guess,x),x);
+  if (goodEnough(guess, x)) {
+    return guess
+  }
+  return sqrtIter(improve(guess, x), x)
 }
 
 const cubeRootIter = (guess, x) => {
-    const goodEnough = (guess,x) => {
-    
-        // return abs(guess*guess*guess - x) <= 0.001;
-        //上面的计算方法在计算较大的数和较小的数的时候会失效
-        return relativeError(guess, improve(guess,x)) < 0.01
-    }
+  const goodEnough = (guess, x) => {
+    // return abs(guess*guess*guess - x) <= 0.001;
+    //上面的计算方法在计算较大的数和较小的数的时候会失效
+    return relativeError(guess, improve(guess, x)) < 0.01
+  }
 
-    const div3 = (x,y) => (x + y) / 3;
-    // 返回新的猜测值
-    const improve = (guess,x) => {
-        return div3(x/(guess*guess), guess*2);
-    }
-    const relativeError = (estimate, reference) => {
-        return abs(estimate-reference) / reference;
-    }
-    if (goodEnough(guess,x)) {
-        return guess;
-    }
-    return cubeRootIter(improve(guess,x),x);
+  const div3 = (x, y) => (x + y) / 3
+  // 返回新的猜测值
+  const improve = (guess, x) => {
+    return div3(x / (guess * guess), guess * 2)
+  }
+  const relativeError = (estimate, reference) => {
+    return abs(estimate - reference) / reference
+  }
+  if (goodEnough(guess, x)) {
+    return guess
+  }
+  return cubeRootIter(improve(guess, x), x)
 }
 
-const sqrt = x => {
-    return sqrtIter(1.0,x)
+const sqrt = (x) => {
+  return sqrtIter(1.0, x)
 }
 
-const cubeRoot = x => {
-    return cubeRootIter(1.0,x)
+const cubeRoot = (x) => {
+  return cubeRootIter(1.0, x)
 }
 
 // 这里的过程有类似的地方,这时应该猜测是否有抽象的可能
@@ -73,13 +71,65 @@ const cubeRoot = x => {
 // 弄清除在设计过程的时候,定义什么,隐藏什么;比如对于goodEnough来说,square与其说是一个过程
 // 不如说是一个抽象过程,在这一层次的抽象上,任何能求出平方的过程都OK,这就是将goodEnough看做黑箱的好处
 
+//计算分钱币的思路
+// 首先，需要组合的钱币金额记为a,钱币的总数为n
+// 如果a == 0, 那么 count = 1; 同理 a < 0, count = 1; n == 0, count = 0
+// 分钱币的时候分为两类,不使用第一种钱币的分类数目 + 使用第一类钱币的分类数目
+
+const count_change = (amount, n) => {
+  if (amount === 0) {
+    return 1
+  }
+  if (amount < 0 || n === 0) {
+    return 0
+  }
+  return (
+    count_change(amount, n - 1) + count_change(amount - numberToAmount(n), n)
+  )
+}
+const count_charge_new = (amount, n, k) => {
+  if (amount === 0) {
+    return k(1)
+  }
+  if (amount < 0 || n === 0) {
+    return k(0)
+  }
+  return count_charge_new(amount, n - 1, (r1) => {
+    return count_charge_new(amount - numberToAmount(n), n, (r2) => {
+      return k(r1 + r2)
+    })
+  })
+}
+
+const numberToAmount = (n) => {
+  if (n === 5) {
+    return 50
+  }
+  if (n === 4) {
+    return 25
+  }
+  if (n === 3) {
+    return 10
+  }
+  if (n === 2) {
+    return 5
+  }
+  if (n === 1) {
+    return 1
+  }
+}
+
+const countChange = (amount) => count_change(amount, 5)
+
 module.exports = {
-    abs,
-    greaterOrEqual,
-    plus,
-    minus,
-    a_plus_abs_b,
-    sqrtIter,
-    sqrt,
-    cubeRoot
+  abs,
+  greaterOrEqual,
+  plus,
+  minus,
+  a_plus_abs_b,
+  sqrtIter,
+  sqrt,
+  cubeRoot,
+  countChange,
+  count_charge_new,
 }
--- src/sicp/math/math.test.js
+++ src/sicp/math/math.test.js
@@ -1,44 +1,52 @@
 import {
-    abs,
-    greaterOrEqual,
-    plus,
-    minus,
-    a_plus_abs_b,
-    sqrtIter,
-    sqrt,
-    cubeRoot
-} from './math';
+  abs,
+  greaterOrEqual,
+  plus,
+  minus,
+  a_plus_abs_b,
+  sqrtIter,
+  sqrt,
+  cubeRoot,
+  countChange,
+  count_charge_new,
+} from './math'
 
-describe('math test',()=>{
-    it('a_plus_abs_b test',()=>{
-        expect(a_plus_abs_b(5,-4)).toEqual(9);
-        expect(plus(5,-4)).toEqual(1);
-        expect(minus(5,-4)).toEqual(9);
-        expect(greaterOrEqual(5,-4)).toBeTruthy();
-        expect(abs(-4)).toEqual(4);
-    });
+describe('math test', () => {
+  it('a_plus_abs_b test', () => {
+    expect(a_plus_abs_b(5, -4)).toEqual(9)
+    expect(plus(5, -4)).toEqual(1)
+    expect(minus(5, -4)).toEqual(9)
+    expect(greaterOrEqual(5, -4)).toBeTruthy()
+    expect(abs(-4)).toEqual(4)
+  })
 
-    const p = ()=>p();
-    const test = (x,y)=>x === 0 ? 0: y;
-    it('js applicative order or nornal order',()=>{
-        // applicative order
-        expect(()=>{
-            test(0, p());
-        }).toThrowError('Maximum call stack size exceeded')
-    });
+  const p = () => p()
+  const test = (x, y) => (x === 0 ? 0 : y)
+  it('js applicative order or nornal order', () => {
+    // applicative order
+    expect(() => {
+      test(0, p())
+    }).toThrowError('Maximum call stack size exceeded')
+  })
 
-    it('sqrtIter test',()=>{
-        expect(sqrtIter(1,2)).toBeGreaterThan(1.414);
-        expect(sqrtIter(1,9)).toBeGreaterThan(3);
-        // 这里的值总是要比精确值大一点点
-        expect(sqrt(16)).toBeGreaterThan(4);
+  it('sqrtIter test', () => {
+    expect(sqrtIter(1, 2)).toBeGreaterThan(1.414)
+    expect(sqrtIter(1, 9)).toBeGreaterThan(3)
+    // 这里的值总是要比精确值大一点点
+    expect(sqrt(16)).toBeGreaterThan(4)
 
-        expect(sqrt(0.0001)).toBeGreaterThan(0.01);
+    expect(sqrt(0.0001)).toBeGreaterThan(0.01)
 
-        expect(sqrt(1000000)).toBeGreaterThan(1000);
-    });
-    it('cubeRoot test',()=>{
-        expect(cubeRoot(27)).toBeGreaterThan(3);
-        expect(cubeRoot(4.5)).toBeGreaterThan(1.658);
-    })
-});
+    expect(sqrt(1000000)).toBeGreaterThan(1000)
+  })
+  it('cubeRoot test', () => {
+    expect(cubeRoot(27)).toBeGreaterThan(3)
+    expect(cubeRoot(4.5)).toBeGreaterThan(1.658)
+  })
+  it('countChange test', () => {
+    expect(countChange(100)).toEqual(292)
+  })
+  it('count_charge_new test', () => {
+    expect(count_charge_new(100, 5, (x) => x)).toEqual(292)
+  })
+})
