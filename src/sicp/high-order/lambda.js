ae2fd3872607e39967a0e5f3f138316069117836
yun900800@126.com <yun900800@126.com>
2024-06-28 21:05:01 +0800

    [feature] 完成第一章最后的练习

--- src/sicp/high-order/lambda.js
+++ src/sicp/high-order/lambda.js
@@ -28,7 +28,7 @@ const fixedPoint = (f, initGuess)=>{
     const closeEnough = (x,y) => Math.abs(x-y)<0.00001; 
     const tryGuess = guess=>{
         const next = f(guess);
-        console.log('sequences',next);
+        // console.log('sequences',next);
         return closeEnough(next, guess)
             ? next
             : tryGuess(next);
@@ -38,8 +38,108 @@ const fixedPoint = (f, initGuess)=>{
 
 const sqrt = x =>fixedPoint(y=>(y+x/y)/2,1.0);
 
+const contFrac = (n,d,k) =>{
+    const fraction = i =>{
+        return i > k ? 0
+            : n(i) / (d(i)+ fraction(i+1))
+    }
+    return fraction(1);
+}
+
+// 在这里的迭代中 result = n(i)/(d(i)+ result) 这个部分保持不变
+const contFracIter = (n,d,k) =>{
+    const fraction = (i,result)=>{
+        return i == 0 
+            ? result
+            : fraction(i-1, n(i)/(d(i)+ result));
+    }
+    return fraction(k,0)
+}
+
+const tanCf = (x,k) =>{
+    return contFrac(i=>i==1?x: -x*x,
+        i=>2*i-1,
+        k
+    );
+}
+
+const averageDamp = f => {
+    return x=>(x+f(x))/2;
+}
+
+const sqrtNew = x => {
+    return fixedPoint(averageDamp(y=>x/y),1.0);
+}
+
+const cubeRoot = x => {
+    return fixedPoint(averageDamp(y=>x/(y*y)),1.0);
+}
+
+const deriv = g=> {
+    const dx = 0.00001;
+    return x=> (g(x+dx) - g(x))/dx;
+}
+
+const newtonTransform = g =>{
+    return x=> x- g(x)/deriv(g)(x)
+}
+
+const newtonMethod = (g,guess)=>{
+    return fixedPoint(newtonTransform(g),guess);
+}
+
+const sqrtNewton = x => {
+    return newtonMethod(y=>y*y-x,1);
+}
+
+const fixedPointOfTransform = (g,transform,guess)=>{
+    return fixedPoint(transform(g), guess);
+}
+
+const compose = (f,g)=>x=>f(g(x));
+
+// repeated(f,1) = f(x);
+// repeated(f,2) = f(f(x));
+// repeated(f,3) = f(f(f(x))); ==f(repeat(f,2)) 所以 repeat(f,k) = f(repeat(f,k-1))= compose(f,repeat(f,k-1))；
+// repeated(f,0) = x;
+const repeated = (f,n)=>{
+    return n===0?
+        x=>x
+        : compose(f,repeated(f,n-1));
+}
+
+const smooth = f=>{
+    const dx = 0.00001;
+    return x => (f(x-dx)+f(x)+f(x+dx))/3;
+}
+
+const nFoldSmooth = (f,n)=>{
+    return repeated(smooth,n)(f);
+}
+
+const iterateImprove = (isGoodEnough,improve)=>{
+    const iterate = x=>{
+        return isGoodEnough(x)
+            ? x
+            : iterate(improve(x));
+    }
+    return iterate;
+}
+
+
 module.exports = {
     halfIntervalMethod,
     fixedPoint,
-    sqrt
+    sqrt,
+    contFrac,
+    contFracIter,
+    tanCf,
+    sqrtNew,
+    cubeRoot,
+    deriv,
+    newtonMethod,
+    sqrtNewton,
+    compose,
+    repeated,
+    iterateImprove
 }
--- src/sicp/high-order/lambda.test.js
+++ src/sicp/high-order/lambda.test.js
@@ -1,7 +1,17 @@
 import {
     halfIntervalMethod,
     fixedPoint,
-    sqrt
+    sqrt,
+    contFrac,
+    contFracIter,
+    tanCf,
+    sqrtNew,
+    cubeRoot,
+    deriv,
+    sqrtNewton,
+    newtonMethod,
+    compose,
+    repeated
 } from './lambda';
 describe('lambda test',()=>{
     it('halfIntervalMethod test',()=>{
@@ -24,8 +34,65 @@ describe('lambda test',()=>{
         value = fixedPoint(x=>Math.log(1000)/ Math.log(x), 2.0);
         console.log('value2',value);
     });
-    it('sqrt test',()=>{
-        const value = sqrt(2.0);
+    it('sqrt sqrtNew test',()=>{
+        let value = sqrt(2.0);
+        expect(value).toBeGreaterThan(1.414);
+
+        value = sqrtNew(2.0);
+        expect(value).toBeGreaterThan(1.414);
+    });
+
+    it('contFrac test',()=>{
+        let value = contFrac(i=>1,i=>1,11);
+        expect(value).toBeGreaterThan(0.618);
+        value = contFracIter(i=>1,i=>1,11);
+        expect(value).toBeGreaterThan(0.618);
+
+        value = 2 + contFrac(i => 1,  
+            i => (i + 1) % 3 < 1 ? 2 * (i + 1) / 3 : 1,
+            20);
+        expect(value).toBeGreaterThan(2.718);
+    });
+    it('tanCf test',()=>{
+        const value = tanCf(Math.PI/4,10);
+        expect(value).toEqual(1);
+    });
+
+    it('cubeRoot test',()=>{
+        const value = cubeRoot(7.8);
+        expect(value).toBeLessThan(2.0)
+    });
+
+    it('deriv test',()=>{
+        const cube = x=> x*x*x;
+        const result = deriv(cube)(5);
+        expect(result).toBeGreaterThan(75)
+    });
+
+    it('sqrtNewton test',()=>{
+        const value = sqrtNewton(2);
         expect(value).toBeGreaterThan(1.414)
+    });
+
+    it('newtonMethod test',()=>{
+        const cubic = (a,b,c) => x=> x*x*x + a*x*x+ b*x+c;
+        const value = newtonMethod(cubic(1,1,1),1.0);
+        console.log('value',value)
+    });
+
+    it('double test',()=>{
+        const double = f=>{
+            return x=>f(f(x));
+        }
+        const inc = x=>x+1;
+        const result = double(double(double))(inc)(5);
+        console.log('result',result);
+    });
+
+    it('compose test',()=>{
+        const result = compose(x=>x*x,x=>x+1)(6);
+        console.log('result',result);
+        const result1 = repeated(x=>x*x,2)(5);
+        expect(result1).toEqual(625)
     });
 })
